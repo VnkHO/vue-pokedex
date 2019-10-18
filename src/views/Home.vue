9b13652618deb9fd10967242c98028abc948cdf2
VnkHO <ho.vincent.nk@gmail.com>
2019-10-18 20:31:56 +0200

    First commit - vuejs vuex

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="home" class="home">
+    <header>
+      <h1>Test</h1>
+    </header>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "home",
+  components: {}
+});
+</script>
+
